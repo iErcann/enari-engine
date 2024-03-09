b047417c3c4f5b01a9fd083dcfdaebeb286adbfe
ercan <ercanfr@protonmail.com>
2024-03-09 18:02:14 +0100

    Fixed

--- src/View/Mesh/FakeSpotLight.ts
+++ src/View/Mesh/FakeSpotLight.ts
@@ -4,69 +4,82 @@ import { Vector3D } from "../../Core/Vector";
 import { DebugUI } from "../DebugUI";
 
 export interface FakeSpotLightParameter {
-    radius?: number;
-    height?: number;
-    color1?: THREE.Color;
-    color2?: THREE.Color;
-    position?: Vector3D;
-    rotation?: THREE.Euler;
-    anglePower?: number;
-    attenuation?: number;
-
+  radius?: number;
+  height?: number;
+  color1?: THREE.Color;
+  color2?: THREE.Color;
+  position?: Vector3D;
+  rotation?: THREE.Euler;
+  anglePower?: number;
+  attenuation?: number;
 }
 export class FakeSpotLight extends THREE.Object3D {
+  static defaultParameter: FakeSpotLightParameter = {
+    radius: 10,
+    height: 16,
+    color1: new THREE.Color(0xffd6a1),
+    color2: new THREE.Color(0xeda342),
+    position: Vector3D.ZERO(),
+    rotation: Vector3D.ZERO().toEuler(),
+    anglePower: 1.0,
+    attenuation: 2.0,
+  };
 
-    static defaultParameter: FakeSpotLightParameter = {
-        radius: 10,
-        height: 16,
-        color1: new THREE.Color(0xffd6a1),
-        color2: new THREE.Color(0xeda342),
-        position: Vector3D.ZERO(),
-        rotation: Vector3D.ZERO().toEuler(),
-        anglePower: 1.0,
-        attenuation: 2.0
-    }
+  private attenuation: number;
+  private anglePower: number;
+  private radius: number;
+  private height: number;
 
-    private attenuation: number;
-    private anglePower: number;
-    private radius: number;
-    private height: number;
+  private uniforms: any;
+  constructor(params: FakeSpotLightParameter = FakeSpotLight.defaultParameter) {
+    super();
+    params = { ...FakeSpotLight.defaultParameter, ...params };
+    const {
+      radius,
+      height,
+      color1,
+      color2,
+      position,
+      rotation,
+      anglePower,
+      attenuation,
+    } = params;
+    this.attenuation = attenuation!;
+    this.anglePower = anglePower!;
+    this.radius = radius!;
+    this.height = height!;
 
-    private uniforms: any;
-    constructor(params: FakeSpotLightParameter = FakeSpotLight.defaultParameter) {
-        super();
-        params = { ...FakeSpotLight.defaultParameter, ...params };
-        const { radius, height, color1, color2, position, rotation, anglePower, attenuation } = params;
-        this.attenuation = attenuation!;
-        this.anglePower = anglePower!;
-        this.radius = radius!;
-        this.height = height!;
+    //const geometry = new THREE.ConeGeometry(radius, height, 32, 20, false);
+    const geometry = new THREE.CylinderGeometry(
+      0.1,
+      radius,
+      height,
+      32 * 2,
+      20,
+      false
+    );
 
+    this.uniforms = {
+      color1: {
+        value: color1,
+      },
+      color2: {
+        value: color2,
+      },
+      anglePower: {
+        value: anglePower,
+      },
+      attenuation: {
+        value: attenuation,
+      },
+      spotPosition: {
+        value: Vector3D.ZERO(),
+      },
+    };
 
-        //const geometry = new THREE.ConeGeometry(radius, height, 32, 20, false);
-        const geometry = new THREE.CylinderGeometry(0.1, radius, height, 32 * 2, 20, false);
-
-        this.uniforms = {
-            color1: {
-                value: color1
-            },
-            color2: {
-                value: color2
-            },
-            anglePower: {
-                value: anglePower
-            },
-            attenuation: {
-                value: attenuation
-            },
-            spotPosition: {
-                value: Vector3D.ZERO()
-            }
-        };
-
-        const material = new THREE.ShaderMaterial({
-            uniforms: this.uniforms,
-            vertexShader: `
+    const material = new THREE.ShaderMaterial({
+      uniforms: this.uniforms,
+      vertexShader: `
                 varying vec3 vNormal;
                 uniform float attenuation;
                 varying vec3 vWorldPosition;
@@ -79,7 +92,7 @@ export class FakeSpotLight extends THREE.Object3D {
                 }
             `,
 
-            fragmentShader: `
+      fragmentShader: `
                 varying float distance;
                 varying vec3 vNormal;
                 uniform vec3 color1;
@@ -95,41 +108,59 @@ export class FakeSpotLight extends THREE.Object3D {
                 }
             `,
 
-            side: THREE.DoubleSide,
-            blending: THREE.AdditiveBlending,
+      side: THREE.DoubleSide,
+      blending: THREE.AdditiveBlending,
 
-            transparent: true,
-            depthWrite: false,
-            depthTest: true
-        });
+      transparent: true,
+      depthWrite: false,
+      depthTest: true,
+    });
 
-        const cone = new THREE.Mesh(geometry, material);
-        material.side = THREE.DoubleSide;
-        this.attach(cone);
+    const cone = new THREE.Mesh(geometry, material);
+    material.side = THREE.DoubleSide;
+    this.attach(cone);
 
-        this.position.copy(position!);
-        this.rotation.copy(rotation!);
-    }
-    public addToDebugUI(debugUI: DebugUI) {
-        const spot = debugUI.addFolder({ title: "Fake SpotLight" });
-        const att = debugUI.addInput(this, "attenuation").on("change", () => {
-            this.uniforms['attenuation'].value = this.attenuation;
-        })
-        const anglePower = debugUI.addInput(this, "anglePower").on("change", () => {
-            this.uniforms['anglePower'].value = this.anglePower;
-        })
-        const rad = debugUI.addInput(this, "radius").on("change", () => {
-            (this.children[0] as any).geometry = new THREE.CylinderGeometry(0.1, this.radius, this.height, 32 * 2, 20, false);
-        })
+    this.position.copy(position!);
+    this.rotation.copy(rotation!);
+  }
+  public addToDebugUI(debugUI: DebugUI) {
+    const spot = debugUI.addFolder({ title: "Fake SpotLight" });
+    const att = debugUI
+      .addInput(this, "attenuation" as any)
+      .on("change", () => {
+        this.uniforms["attenuation"].value = this.attenuation;
+      });
+    const anglePower = debugUI
+      .addInput(this, "anglePower" as any)
+      .on("change", () => {
+        this.uniforms["anglePower"].value = this.anglePower;
+      });
+    const rad = debugUI.addInput(this, "radius" as any).on("change", () => {
+      (this.children[0] as any).geometry = new THREE.CylinderGeometry(
+        0.1,
+        this.radius,
+        this.height,
+        32 * 2,
+        20,
+        false
+      );
+    });
 
-        const height = debugUI.addInput(this, "height").on("change", () => {
-            (this.children[0] as any).geometry = new THREE.CylinderGeometry(0.1, this.radius, this.height, 32 * 2, 20, false);
-        })
-        debugUI.addMesh(this);
-        spot.add(rad);
-        spot.add(height);
+    const height = debugUI.addInput(this, "height" as any).on("change", () => {
+      (this.children[0] as any).geometry = new THREE.CylinderGeometry(
+        0.1,
+        this.radius,
+        this.height,
+        32 * 2,
+        20,
+        false
+      );
+    });
+    debugUI.addMesh(this);
+    spot.add(rad);
+    spot.add(height);
 
-        spot.add(att);
-        spot.add(anglePower);
-    }
+    spot.add(att);
+    spot.add(anglePower);
+  }
 }
